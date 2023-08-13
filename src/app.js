5cb49951fd42f2f946d20955aff30a0fd0711a99
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-08-14 02:12:24 +0500

    second commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
